eefe0d29617d403ddc7dedd40db09ab5caa72177
XP20 <toms.b.gm@gmail.com>
2023-11-28 12:16:19 +0200

    Disable cmp in comments

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -45,6 +45,16 @@ local function border(hl_name)
 end
 
 local options = {
+  preselect = cmp.PreselectMode.None,
+  enabled = function()
+    local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+    if in_prompt then
+      return false
+    end
+    local context = require("cmp.config.context")
+    return not(context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
+  end,
+
   completion = {
     completeopt = "menu,menuone",
   },
